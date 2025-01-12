95fc56437294131cf22b901d0ea928daf4405b65
svgbars75 <daymyo317@gmail.com>
2025-01-13 00:17:37 +0200

    loop body in quotes in row 7 and 9

--- src/sum_number/SumNumber.java
+++ src/sum_number/SumNumber.java
@@ -4,8 +4,9 @@ public class SumNumber {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6};
         int sum = 0;
-        for (int i = 0; i < numbers.length; i++)
-        {System.out.println((i + 1) + ") Num is " + (i + 1) + ", sum is " + (sum = sum + numbers[i]));}
+        for (int i = 0; i < numbers.length; i++) {
+        System.out.println((i + 1) + ") Num is " + (i + 1) + ", sum is " + (sum = sum + numbers[i]));
+        }
         System.out.println("------------------------");
         System.out.println("Sum of numbers is " + sum);
     }
