a7a5debfc4216ba3dcc278daa96fc84b138ecbc9
svgbars75 <daymyo317@gmail.com>
2025-01-13 00:12:34 +0200

    loop body in quotes

--- src/sum_number/SumNumber.java
+++ src/sum_number/SumNumber.java
@@ -5,7 +5,7 @@ public class SumNumber {
         int[] numbers = {1, 2, 3, 4, 5, 6};
         int sum = 0;
         for (int i = 0; i < numbers.length; i++)
-            System.out.println((i + 1) + ") Num is " + (i + 1) + ", sum is " + (sum = sum + numbers[i]));
+        {System.out.println((i + 1) + ") Num is " + (i + 1) + ", sum is " + (sum = sum + numbers[i]));}
         System.out.println("------------------------");
         System.out.println("Sum of numbers is " + sum);
     }
