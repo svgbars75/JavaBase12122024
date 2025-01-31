264e05587763e4c0bd79516111b2c0e70fc4e44f
svgbars75 <daymyo317@gmail.com>
2025-01-31 22:49:47 +0200

    Create unsorted array

--- src/sum_number/hw7_array_handline/hw8_sorting_searching/SortingSearching.java
+++ src/sum_number/hw7_array_handline/hw8_sorting_searching/SortingSearching.java
@@ -1,8 +1,15 @@
 package sum_number.hw7_array_handline.hw8_sorting_searching;
 
+import java.util.Arrays;
+
 public class SortingSearching {
     public static void main(String[] args) {
         int[] sort = new int[15];
+        for (int i = 0; i < sort.length; i++) {
+            sort[i] = (int) (Math.random() * (100)+1);
+        }
+        System.out.println("Элементы несортированного массива:" + Arrays.toString(sort));
+
 
     }
 }
