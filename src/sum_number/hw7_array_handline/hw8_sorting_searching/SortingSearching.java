7e5280d7fc182901fc3a3e80e80b609c84f850e1
svgbars75 <daymyo317@gmail.com>
2025-01-31 20:59:51 +0200

    Initial SortingSearching

---
+++ src/sum_number/hw7_array_handline/hw8_sorting_searching/SortingSearching.java
@@ -0,0 +1,8 @@
+package sum_number.hw7_array_handline.hw8_sorting_searching;
+
+public class SortingSearching {
+    public static void main(String[] args) {
+        int[] sort = new int[15];
+
+    }
+}
